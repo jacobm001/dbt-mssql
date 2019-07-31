744fa752eb3c066738252121590af2e17935aa8a
Jacob Mastel <jacob.mastel@oregonstate.edu>
2019-07-31 15:11:34 -0700

    Fixed seed insertion
    pyodbc doesn't like using the `%s` parameter notation. That has now been replaced with the `?` notation.

---
+++ dbt/include/mssql/macros/materializations/seed/seed.sql
@@ -0,0 +1,35 @@
+{% macro basic_load_csv_rows(model, batch_size) %}
+    {% set agate_table = model['agate_table'] %}
+    {% set cols_sql = ", ".join(agate_table.column_names) %}
+    {% set bindings = [] %}
+
+    {% set statements = [] %}
+
+    {% for chunk in agate_table.rows | batch(batch_size) %}
+        {% set bindings = [] %}
+
+        {% for row in chunk %}
+            {% set _ = bindings.extend(row) %}
+        {% endfor %}
+
+        {% set sql %}
+            insert into {{ this.render(False) }} ({{ cols_sql }}) values
+            {% for row in chunk -%}
+                ({%- for column in agate_table.column_names -%}
+                    ?
+                    {%- if not loop.last%},{%- endif %}
+                {%- endfor -%})
+                {%- if not loop.last%},{%- endif %}
+            {%- endfor %}
+        {% endset %}
+
+        {% set _ = adapter.add_query(sql, bindings=bindings, abridge_sql_log=True) %}
+
+        {% if loop.index0 == 0 %}
+            {% set _ = statements.append(sql) %}
+        {% endif %}
+    {% endfor %}
+
+    {# Return SQL so we can render it out into the compiled files #}
+    {{ return(statements[0]) }}
+{% endmacro %}
