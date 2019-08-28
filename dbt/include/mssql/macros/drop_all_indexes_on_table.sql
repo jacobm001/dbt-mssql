6b20dacf858463ce6918ad8034490c8a1b6fe051
curious-bryan <32725506+curious-bryan@users.noreply.github.com>
2019-08-28 13:55:36 -0500

    Create drop_all_indexes_on_table.sql
    This code was adapted from Stackoverflow answer at https://stackoverflow.com/q/1344401/10415173.
    
    Code follows these steps
    
    1. Drop all XML indexes (if any)
    2. Drop all spatial indexes (if any)
    3. Drop all foreign keys pointing to table (if any)
    4. Drop all primary key constraints (if any)
    5. Drop all remaining indexes
    
    Uses these system objects on MSSQL db:
    - sys.indexes
    - sys.tables
    - sys.foreign_keys
    - sp_executesql

---
+++ dbt/include/mssql/macros/drop_all_indexes_on_table.sql
@@ -0,0 +1,61 @@
+{% macro drop_all_indexes_on_table() %}
+{# Altered from https://stackoverflow.com/q/1344401/10415173 #}
+
+declare @drop_xml_indexes_first nvarchar(max);
+select @drop_xml_indexes_first = ( 
+		select 'DROP INDEX IF EXISTS [' + sys.indexes.[name] + '] ON ' + '[' + SCHEMA_NAME(sys.tables.[schema_id]) + '].[' + OBJECT_NAME(sys.tables.[object_id]) + ']; '
+		from sys.indexes 
+		inner join sys.tables on sys.indexes.object_id = sys.tables.object_id
+		where sys.indexes.[name] is not null 
+		  and sys.indexes.type_desc = 'XML'
+		  and sys.tables.[name] = '{{ this.table }}'
+		for xml path('')
+	);
+exec sp_executesql @drop_xml_indexes_first;
+
+declare @drop_spatial_indexes_second nvarchar(max);
+select @drop_spatial_indexes_second = ( 
+		select 'DROP INDEX IF EXISTS [' + sys.indexes.[name] + '] ON ' + '[' + SCHEMA_NAME(sys.tables.[schema_id]) + '].[' + OBJECT_NAME(sys.tables.[object_id]) + ']; '
+		from sys.indexes 
+		inner join sys.tables on sys.indexes.object_id = sys.tables.object_id
+		where sys.indexes.[name] is not null 
+		  and sys.indexes.type_desc = 'Spatial'
+		  and sys.tables.[name] = '{{ this.table }}'
+		for xml path('')
+	);
+exec sp_executesql @drop_spatial_indexes_second;
+
+declare @drop_fk_constraints_next nvarchar(max);
+select @drop_fk_constraints_next = ( 
+		select 'ALTER TABLE [' + SCHEMA_NAME(sys.foreign_keys.[schema_id]) + '].[' + OBJECT_NAME(sys.foreign_keys.[parent_object_id]) + '] DROP CONSTRAINT IF EXISTS [' + sys.foreign_keys.[name]+ '];'
+		from sys.foreign_keys 
+		inner join sys.tables on sys.foreign_keys.[referenced_object_id] = sys.tables.[object_id]
+		where sys.tables.[name] = '{{ this.table }}'
+		for xml path('') 
+	); 
+exec sp_executesql @drop_fk_constraints_next;
+
+declare @drop_pk_constraints_next nvarchar(max);
+select @drop_pk_constraints_next = ( 
+		select 'ALTER TABLE [' + SCHEMA_NAME(sys.tables.[schema_id]) + '].[' + sys.tables.[name] + '] DROP CONSTRAINT IF EXISTS [' + sys.indexes.[name]+ '];'
+		from sys.indexes 
+		inner join sys.tables on sys.indexes.[object_id] = sys.tables.[object_id]
+		where sys.indexes.is_primary_key = 1
+		  and sys.tables.[name] = '{{ this.table }}'
+		for xml path('') 
+	);
+exec sp_executesql @drop_pk_constraints_next;
+
+declare @drop_remaining_indexes_last nvarchar(max);
+select @drop_remaining_indexes_last = 
+	(
+		select 'DROP INDEX IF EXISTS [' + sys.indexes.[name] + '] ON ' + '[' + SCHEMA_NAME(sys.tables.[schema_id]) + '].[' + OBJECT_NAME(sys.tables.[object_id]) + ']; '
+		from sys.indexes 
+		inner join sys.tables on sys.indexes.object_id = sys.tables.object_id
+		where sys.indexes.[name] is not null 
+		  and sys.tables.[name] = '{{ this.table }}'
+		for xml path('')
+	);
+exec sp_executesql @drop_remaining_indexes_last;
+
+{% endmacro %}
